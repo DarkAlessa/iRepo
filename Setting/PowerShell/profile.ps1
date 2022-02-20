784c8df0fc62546e76e87b2ebe8160138fa276cb
DarkAlessa <skykick.013@gmail.com>
2022-02-20 13:43:06 +0700

    powershell prompt color

--- Setting/PowerShell/profile.ps1
+++ Setting/PowerShell/profile.ps1
@@ -1,7 +1,6 @@
-function Prompt
-{
+function Prompt {
     $esc=$([char]27) # ESC or `e in ASCII table
     $promtSymbol = ""
     $currentDir = (Get-Location | Get-Item).Name
-    "$esc[38;2;255;255;255;48;2;255;36;20m $env:COMPUTERNAME $esc[0m$esc[38;2;255;255;255;48;2;255;36;20m$promtSymbol$esc[0m$esc[38;2;255;255;255;48;2;255;36;20m Windows $esc[0m$esc[38;2;255;36;20;48;2;255;176;50m$esc[0m$esc[38;2;50;50;50;48;2;255;176;50m $currentDir $esc[0m$esc[38;2;255;176;50m $esc[0m"
+    "$esc[38;2;255;255;255;48;2;70;70;70m $env:COMPUTERNAME $esc[0m$esc[38;2;255;255;255;48;2;70;70;70m$promtSymbol$esc[0m$esc[38;2;255;255;255;48;2;70;70;70m Windows $esc[0m$esc[38;2;70;70;70;48;2;255;176;50m$esc[0m$esc[38;2;50;50;50;48;2;255;176;50m $currentDir $esc[0m$esc[38;2;255;176;50m $esc[0m"
 }
